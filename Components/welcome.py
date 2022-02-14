b571b0fce44bedce87dafe1309728c0b3e73f737
jhkfgjflkdjg <99223118+jhkfgjflkdjg@users.noreply.github.com>
2022-02-15 10:24:44 +1300

    Created component file
    Created welcome message with random name generator

---
+++ Components/welcome.py
@@ -0,0 +1,13 @@
+import random
+from random import randint 
+
+# List of random names 
+names =["Mikara","Jodek","Tomas","Carlos","Karlo","Louis","Joaquin","Shawn","Marcus","Gabe"]
+
+num = randint(0,9)
+
+name = (names[num])
+
+print("*** Welcome to Dream Pizza***")
+print("*** My name is",name, "***")
+print("*** I will be here to help you order your delicious Dream Pizza***")
--- pizza_bot.py
+++ pizza_bot.py
@@ -1,3 +1,1 @@
 # pizza bot program
-
-name=["Mikara","Jodek","Tomas","Carlos","Karlo","Louis","Joaquin","Shawn","Marcus","Gabe"]
