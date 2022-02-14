280fc311e95a640ec4f39a3defc65383bbe9c1ae
jhkfgjflkdjg <99223118+jhkfgjflkdjg@users.noreply.github.com>
2022-02-15 11:04:28 +1300

    Created welcome v2
    Change the welcome message code into a function. Also created a main() function to call welcome()

---
+++ Components/welcome v2.py
@@ -0,0 +1,31 @@
+import random
+from random import randint 
+
+# List of random names 
+names =["Mikara","Jodek","Tomas","Carlos","Karlo","Louis","Joaquin","Shawn","Marcus","Gabe"]
+
+def welcome():
+    ''' 
+    Purpose: To generate a random name from the list and print out 
+    a welcome name 
+    parameter: None
+    Returns: None
+    '''
+    num = randint(0,9)
+    name = (names[num])
+    print("*** Welcome to Dream Pizza***")
+    print("*** My name is",name, "***")
+    print("*** I will be here to help you order your delicious Dream Pizza***")
+
+
+def main():
+    ''' 
+    Purpose: To run all fuctions 
+    a welcome message 
+    parameter: None
+    Returns: None
+    '''
+    welcome()
+
+
+main ()
