27f0f2c5341d2bbcd074e8bb53a1e17fdc92b96f
jhkfgjflkdjg <99223118+jhkfgjflkdjg@users.noreply.github.com>
2022-02-27 16:15:51 +1300

    Updated pizza_bot
    Title class for not blank return.

--- pizza_bot.py
+++ pizza_bot.py
@@ -4,11 +4,24 @@ from random import randint
 
 # List of random names 
 names =["Mikara","Jodek","Tomas","Carlos","Karlo","Louis","Joaquin","Shawn","Marcus","Gabe"]
+#customer detalis dictionary
+customer_details = {}
+
+#Validate inputs to check if they are blank
+def not_blank(question):
+    valid = False 
+    while not valid:
+        response = input(question)
+        if response != "":
+            return response.title()
+        else:
+            print("This cannot be blank")
+
 
 def welcome():
     ''' 
     Purpose: To generate a random name from the list and print out 
-    a welcome name 
+    a welcome message
     parameter: None
     Returns: None
     '''
@@ -18,9 +31,9 @@ def welcome():
     print("*** My name is",name, "***")
     print("*** I will be here to help you order your delicious Dream Pizza***")
 
-  #main for pickup and delivery 
+#menu for pickup and delivery 
 
-def pickup(): 
+def order_type(): 
  print ("Is your order for pickup or delivery?")
 
 print ("For pickup please enter 1")
@@ -30,11 +43,11 @@ while True:
      delivery = int(input("please enter a number"))
      if delivery >= 1 and delivery <= 2:  
         if delivery == 2:
-            print ("Delivery")
-            break 
+            print ("pickup")
+            break
         
         elif delivery == 1:
-            print ("Pickup")
+            print ("delivery")
             break 
      else:
          print ("Number must be 1 or 2")
@@ -43,9 +56,14 @@ while True:
       print("please enter 1 or 2")
 
 #pick up information- name and phone number 
-
-
-
+def order_type():
+    question = ("please enter your name ")
+    customer_details['name'] = not_blank(question )
+    print (customer_details['name']) 
+        
+    question = ("please enter your phone number")
+    customer_details ['phone'] = not_blank(question )
+    print (customer_details ['phone'])
 
 # Delivery information - name address and phone
 
@@ -79,7 +97,7 @@ def main():
     Returns: None
     '''
     welcome()
-    pickup()
+    order_type()
     
 
 main ()
